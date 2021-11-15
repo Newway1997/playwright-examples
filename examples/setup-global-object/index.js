323499544612443e9d17cc27394dffdb90acd53d
Newway1997 <573491019@qq.com>
2021-11-15 21:13:39 +0800

    20211115 comment 增加attach形式的debug模式；增加对cefSharp的支持

---
+++ examples/setup-global-object/index.js
@@ -0,0 +1,25 @@
+const { chromium } = require("playwright");
+const path = require("path");
+
+(async function () {
+    //桌面应用标记设为true，表示内嵌的chromium
+    global.isDesktopApplication = true;
+    const browser = await chromium.connectOverCDP("http://localhost:8080");
+    const contexts = await browser.contexts();
+    //打开新的tab
+    const pages = await contexts[0].pages();
+
+    const page = pages.find(async (page) => {
+        return (await page.title()) !== "DevTools";
+    });
+
+    await page.addInitScript(() => {
+        window.go = function () {
+            console.log("hello");
+        };
+    });
+    await page.goto("http://www.bilibili.com");
+
+    await page.screenshot({ path: path.resolve(__dirname, "screenshot.png") });
+    await page.pause();
+})();
